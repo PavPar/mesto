b5788f3e56e2e064d70e66f4029668c0f2b5770d
PavPar <ParamonPav@yandex.ru>
2020-04-16 01:15:48 +0300

    feat: Add all interaction like in task

---
+++ js/popup/popup.js
@@ -0,0 +1,44 @@
+let popup = document.querySelector(".popup");
+let profile = document.querySelector(".profile");
+
+let popup__button = {
+    exit: popup.querySelector(".popup__button_type-exit"),
+    save: popup.querySelector(".popup__button_type-save")
+}
+
+let popup__input = {
+    name: popup.querySelector(".popup__input_type-name"),
+    job: popup.querySelector(".popup__input_type-job")
+}
+
+let profile__button = {
+    edit: profile.querySelector(".profile__button_type-edit")
+}
+
+let profile__data = {
+    name: profile.querySelector(".profile__title"),
+    job: profile.querySelector(".profile__subtitle")
+}
+
+function showPopup() {
+    popup.classList.add("popup_status-opened");
+    popup.style.display = "block";
+    popup__input.name.setAttribute('value',profile__data.name.textContent);
+    popup__input.job.setAttribute('value',profile__data.job.textContent);    
+}
+
+function hidePopup() {
+    popup.classList.remove("popup_status-opened");
+    popup.style.display = "none";
+}
+
+function setData(e){
+    e.preventDefault();
+    profile__data.name.textContent = popup__input.name.value;
+    profile__data.job.textContent = popup__input.job.value;
+    hidePopup();
+}
+
+profile__button.edit.addEventListener("click", showPopup);
+popup__button.exit.addEventListener("click", hidePopup);
+popup__button.save.addEventListener("click",setData);
