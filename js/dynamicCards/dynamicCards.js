4fc4cb278c8a6b22d992b782a977e53f03a09a09
PavPar <ParamonPav@yandex.ru>
2020-05-10 18:34:22 +0300

    fix: Check for errors

--- js/dynamicCards/dynamicCards.js
+++ js/dynamicCards/dynamicCards.js
@@ -36,12 +36,19 @@ function createCard(title, imageLink, alt = title) {
     newCard.querySelector('.card__title').textContent = title;
     newCard.querySelector('.card__image').setAttribute('src',imageLink);
     newCard.querySelector('.card__image').setAttribute('alt',alt);
+    
     newCard.querySelector('.card__button_type-like').addEventListener('click',(e)=>{
         if(event.target.classList.contains('card__button_type-like')){
             event.target.classList.toggle('card__button_state-selected');
         }
     })
     
+    newCard.querySelector('.card__button_type-delete').addEventListener('click',(e)=>{
+        if(event.target.classList.contains('card__button_type-delete')){
+            cards_area.removeChild(event.target.parentNode);
+        }
+    })
+
     cards_area.append(newCard);
 }
 
@@ -52,6 +59,6 @@ initialCards.forEach((element)=>{
 
 
 btn_createCard.addEventListener('click', (e) => {
-    // createCard("test", 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg');
+    createCard("test", 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg');
 })
 
