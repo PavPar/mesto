c7bea77099638328ad41db9c483237946999d655
PavPar <ParamonPav@yandex.ru>
2020-08-14 15:45:17 +0300

    feat: stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './pages/index.css';
+
 import Card from './components/Card.js'; // Класс card для создания карточек
 import FormValidator from './components/FormValidator.js'; //Класс для валидации формы
 import Section from './components/Section.js'; // Класс для размещения объектов на странице
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 // webpack.config.js
 const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -8,12 +10,30 @@ module.exports = {
         filename: 'main.js'
     },
     module: {
-        rules: [ 
-          {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: '/node_modules/'
-          }
-          ]
-      }
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff2|woff|otf|ttf)$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: [MiniCssExtractPlugin.loader, 'css-loader'],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html' // путь к файлу index.html
+        }),
+        new MiniCssExtractPlugin()
+    ]
 }
