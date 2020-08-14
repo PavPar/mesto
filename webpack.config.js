8bd2b5f5f0b8cd7c6d1d55327800a74292bb5d27
PavPar <ParamonPav@yandex.ru>
2020-08-14 13:59:40 +0300

    feat: install babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-    }
+    },
+    module: {
+        rules: [ 
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+          }
+          ]
+      }
 }
